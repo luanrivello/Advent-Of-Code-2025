64e071d94e27c01ff67c0d26c4cd7712a7ebd163
Luan Belem Rivello <luanrivello@hotmail.com>
2025-12-07 10:40:36 -0300

    Day06 Part01

--- src/main/kotlin/day06/Main.kt
+++ src/main/kotlin/day06/Main.kt
@@ -11,14 +11,50 @@ fun main() {
         val inputFile = readFile("input.txt")
         val lines = inputFile.readLines()
 
+        val worksheet: List<List<String>> = lines.map {
+            it.split(" ")
+              .filter { it.isNotBlank() }
+        }
+
+        worksheet.printResult()
+        answer = worksheet.doTheMath()
     }
 
     println("Execution time: ${time / 1_000_000_000.0}s")
     println("Answer: $answer")
 }
 
+private fun List<List<String>>.doTheMath(): Long {
+    var result = 0L
+
+    this[0].indices.forEach { column ->
+        var problemResult: Long
+        if (this.last()[column] == "*") {
+            problemResult = 1L
+            this.subList(0, size-1).forEach {
+                problemResult *= it[column].toLong()
+            }
+        } else {
+            problemResult = this.subList(0, size-1).sumOf {
+                it[column].toLong()
+            }
+        }
+        result += problemResult
+    }
+
+    return result
+}
+
 private fun readFile(file: String): File {
     val day = object {}::class.java.packageName.substringAfterLast('.')
     val inputURL = {}.javaClass.getResource("/$day/$file")
     return File(inputURL.toURI())
+}
+
+fun List<List<String>>.printResult() {
+    for ((num, row) in this.withIndex()) {
+        val line = row
+        //val line = row.joinToString(" ")
+        println("$num | $line")
+    }
 }
