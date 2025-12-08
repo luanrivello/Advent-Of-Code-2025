7f6eb5c326fc2060554e5131b9042c1a51b15d5d
Luan Belem Rivello <luanrivello@hotmail.com>
2025-12-08 12:48:52 -0300

    Day06 Part02

--- src/main/kotlin/day06/Main.kt
+++ src/main/kotlin/day06/Main.kt
@@ -11,34 +11,84 @@ fun main() {
         val inputFile = readFile("input.txt")
         val lines = inputFile.readLines()
 
-        val worksheet: List<List<String>> = lines.map {
-            it.split(" ")
-              .filter { it.isNotBlank() }
-        }
+        val (worksheet, operators) = extractProblems(lines)
 
-        worksheet.printResult()
-        answer = worksheet.doTheMath()
+        //worksheet.printResult()
+        //answer = worksheet.doTheMath(operators)
+        answer = worksheet.doTheMathWithInt(operators)
     }
 
     println("Execution time: ${time / 1_000_000_000.0}s")
     println("Answer: $answer")
 }
 
-private fun List<List<String>>.doTheMath(): Long {
+private fun extractProblems(worksheet: List<String>): Pair<List<List<Int>>, List<Char>>  {
+    val maxLength = worksheet.maxOf { it.length }
+
+    val paddedSpaces = worksheet
+        .dropLast(1)
+        .map { line ->
+            line + " ".repeat(maxLength - line.length)
+        }
+    //println(paddedSpaces.toString()
+
+    val problemsStr = mutableListOf(mutableListOf<String>())
+    for (col in paddedSpaces[0].indices) {
+        val num = paddedSpaces.fold("") { res, string ->
+            res + string[col]
+        }
+
+        if (num.isBlank()){
+            problemsStr.add(mutableListOf())
+        } else {
+            problemsStr.last().add(num)
+        }
+    }
+    //problemsStr.printResult()
+
+    val problems = problemsStr.map { problem ->
+        problem.map { num ->  num.trim().toInt() }
+    }
+    //println(problemsInt.toString())
+
+    val operators = worksheet
+        .last()
+        .toCharArray()
+        .filter { !it.isWhitespace() }
+
+    return problems to operators
+}
+
+private fun List<List<Int>>.doTheMathWithInt(operators: List<Char>): Long {
     require(isNotEmpty()) { "Matrix must not be empty" }
 
-    val dataRows = this.subList(0, size - 1)
-    val opRow = last()
+    return this.withIndex().sumOf { (col, problem) ->
+        if (operators[col] == '*') {
+            problem.fold(1L) { acc, num ->
+                acc * num.toLong()
+            }
+
+        } else if (operators[col] == '+') {
+            problem.sumOf { num -> num.toLong() }
+
+        } else {
+            error("Unsuported operation")
+        }
+    }
+}
+
+private fun List<List<String>>.doTheMathWithStr(operators: List<String>): Long {
+    require(isNotEmpty()) { "Matrix must not be empty" }
 
     return this[0].indices.sumOf { column ->
-        if (opRow[column] == "*") {
-            dataRows.fold(1L) { mult, row ->
-                mult * row[column].toLong()
+        if (operators[column] == "*") {
+            this.fold(1L) { acc, problem ->
+                acc * problem[column].toLong()
             }
-        } else if (opRow[column] == "+") {
-            dataRows.sumOf {
-                it[column].toLong()
-            }
+
+        } else if (operators[column] == "+") {
+            this.sumOf { it[column].toLong() }
+
         } else {
             error("Unsuported operation")
         }
